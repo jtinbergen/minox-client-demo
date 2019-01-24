870dd4e80becfd2dce911366417e9d36452c0722
Jaapjan Tinbergen <jaapjan@minox.nl>
2019-01-24 08:59:00 +0100

    Do not refer to acceptance environment

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-const express = require('express');
-const passport = require('passport');
-const superagent = require('superagent');
-const session = require('express-session');
-const OAuth2Strategy = require('passport-oauth2').Strategy;
-const ngrok = require('ngrok');
-const opn = require('opn');
+const express = require("express");
+const passport = require("passport");
+const superagent = require("superagent");
+const session = require("express-session");
+const OAuth2Strategy = require("passport-oauth2").Strategy;
+const ngrok = require("ngrok");
+const opn = require("opn");
 
 // ************************************************************
 // ************************************************************
@@ -14,25 +13,30 @@ const opn = require('opn');
 //
 // ************************************************************
 // ************************************************************
-const MINOXDOMAIN = 'https://acc.minox.nl';
-let CLIENTDOMAIN = '';
-const CLIENTID = '';
-const CLIENTSECRET = '';
-const REQUESTEDSCOPES = 'administration:read supplier:read customer:read ledger_account:read journal:read vat:read transaction:read transaction:write';
+const MINOXDOMAIN = "https://app.minox.nl";
+let CLIENTDOMAIN = "";
+const CLIENTID = "";
+const CLIENTSECRET = "";
+const REQUESTEDSCOPES =
+  "administration:read supplier:read customer:read ledger_account:read journal:read vat:read transaction:read transaction:write";
 
-process.on('unhandledRejection', (error) => {
+process.on("unhandledRejection", error => {
   console.error(error);
 });
 
 if (!CLIENTSECRET || !CLIENTID) {
-  console.log('The application cannot start until CLIENTSECRET and CLIENTID are set.');
+  console.log(
+    "The application cannot start until CLIENTSECRET and CLIENTID are set."
+  );
   process.exit(-1);
 }
 
 const app = express();
-app.use(session({
+app.use(
+  session({
     secret: CLIENTSECRET
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -59,66 +63,91 @@ const getProfile = async accessToken => {
 };
 
 const minoxStrategy = new OAuth2Strategy(
-    {
-      authorizationURL: `${MINOXDOMAIN}/oauth/authorize`,
-      tokenURL: `${MINOXDOMAIN}/oauth/token`,
-      clientID: CLIENTID,
-      clientSecret: CLIENTSECRET,
-      callbackURL: `${CLIENTDOMAIN}/connect/minox/success`,
-      scope: REQUESTEDSCOPES
-    },
-    async function(accessToken, refreshToken, profile, callback) {
-      return callback(null, { token: accessToken, ...(await getProfile(accessToken)) });
-    }
-  );
+  {
+    authorizationURL: `${MINOXDOMAIN}/oauth/authorize`,
+    tokenURL: `${MINOXDOMAIN}/oauth/token`,
+    clientID: CLIENTID,
+    clientSecret: CLIENTSECRET,
+    callbackURL: `${CLIENTDOMAIN}/connect/minox/success`,
+    scope: REQUESTEDSCOPES
+  },
+  async function(accessToken, refreshToken, profile, callback) {
+    return callback(null, {
+      token: accessToken,
+      ...(await getProfile(accessToken))
+    });
+  }
+);
 
 passport.use(minoxStrategy);
 
 const authenticated = (request, response, next) => {
-    if (request.user) {
-        next();
-        return;
-    }
+  if (request.user) {
+    next();
+    return;
+  }
 
-    response.redirect('/connect/minox');
+  response.redirect("/connect/minox");
 };
 
 const getAdministrations = async (token, tenantId) => {
-    const reply = await superagent
-        .get(`${MINOXDOMAIN}/api/1/tenant/${tenantId}/administration`)
-        .set({
-            'authorization': `bearer ${token}`
-        });
-    return reply.body;
-}
+  const reply = await superagent
+    .get(`${MINOXDOMAIN}/api/1/tenant/${tenantId}/administration`)
+    .set({
+      authorization: `bearer ${token}`
+    });
+  return reply.body;
+};
 
-app.get('/', async (request, response) => {
-  let html = '<!DOCTYPE html><html><head><title>Minox client demo</title></head><body><h1>Welkom bij Minox client demo</h1>';
+app.get("/", async (request, response) => {
+  let html =
+    "<!DOCTYPE html><html><head><title>Minox client demo</title></head><body><h1>Welkom bij Minox client demo</h1>";
   if (request.user && request.user.token) {
     html += '<a href="/dashboard">Dashboard tonen</a>';
   } else {
     html += '<a href="/dashboard">Koppeling met Minox maken</a>';
   }
-  html += '</body></html>';
+  html += "</body></html>";
   response.send(html);
 });
-app.get('/connect/minox', passport.authenticate('oauth2'));
-app.get('/connect/minox/success', passport.authenticate('oauth2', { failureRedirect: '/login' }), (request, response) => { response.redirect('/'); });
-app.get('/dashboard', authenticated, async (request, response) => {
+app.get("/connect/minox", passport.authenticate("oauth2"));
+app.get(
+  "/connect/minox/success",
+  passport.authenticate("oauth2", { failureRedirect: "/login" }),
+  (request, response) => {
+    response.redirect("/");
+  }
+);
+app.get("/dashboard", authenticated, async (request, response) => {
   try {
     let administrations = [];
     for (const tenant of request.user.linked_tenants) {
-      let tenantAdministrations = (await getAdministrations(request.user.token, tenant.tenant_id)).collection;
-      tenantAdministrations = tenantAdministrations.map(administration => ({ ...tenant, ...administration }));
-      administrations.push(...tenantAdministrations)
+      let tenantAdministrations = (await getAdministrations(
+        request.user.token,
+        tenant.tenant_id
+      )).collection;
+      tenantAdministrations = tenantAdministrations.map(administration => ({
+        ...tenant,
+        ...administration
+      }));
+      administrations.push(...tenantAdministrations);
     }
 
-    let html = '<!DOCTYPE html><html><head><title>Minox client demo</title></head><body><h1>Welkom bij Minox client demo</h1><h2>We kunnen met de onderstaande tenants/administraties verbinden</h2><table><thead><tr><th>TenantID</th><th>AdministrationID</th><th>Company name</th><th>Number of periods</th><th>Chamber of commerce</th></tr></thead><tbody>';
-    html += administrations.map(administration => `<tr><td>${administration.tenant_id}</td><td>${administration.id}</td><td>${administration.name || ''}</td><td>${administration.number_of_periods}</td><td>${administration.chamber_of_commerce || ''}</td></tr>`).join('');
-    html += '</tbody></table></body></html>'
+    let html =
+      "<!DOCTYPE html><html><head><title>Minox client demo</title></head><body><h1>Welkom bij Minox client demo</h1><h2>We kunnen met de onderstaande tenants/administraties verbinden</h2><table><thead><tr><th>TenantID</th><th>AdministrationID</th><th>Company name</th><th>Number of periods</th><th>Chamber of commerce</th></tr></thead><tbody>";
+    html += administrations
+      .map(
+        administration =>
+          `<tr><td>${administration.tenant_id}</td><td>${
+            administration.id
+          }</td><td>${administration.name || ""}</td><td>${
+            administration.number_of_periods
+          }</td><td>${administration.chamber_of_commerce || ""}</td></tr>`
+      )
+      .join("");
+    html += "</tbody></table></body></html>";
     response.send(html);
-  }
-  catch (e) {
+  } catch (e) {
     response.send(e);
   }
 });
@@ -127,6 +156,6 @@ app.get('/dashboard', authenticated, async (request, response) => {
   CLIENTDOMAIN = await ngrok.connect();
   app.listen(7788, () => {
     console.log(`Service is up on port 7788. Client domain = ${CLIENTDOMAIN}`);
-    opn('http://localhost:7788');
+    opn("http://localhost:7788");
   });
 })();
